fac5b3ec5e436499494d45077cfc9d1f9e3f4475
master <chain_13918925958@outlook.com>
2021-05-09 10:03:40 +0800

    Small Adjusts

--- Main.py
+++ Main.py
@@ -9,12 +9,12 @@ from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-fig = plt.figure(figsize=(8, 6))
+fig = plt.figure(figsize=(12, 11))
 ax1 = Axes3D(fig)
 plt.rcParams['font.sans-serif'] = ['SimHei']
 plt.rcParams['axes.unicode_minus'] = False
-dc = DataCollector()
-dc.runStudent(1000, 40)
+# dc = DataCollector()
+# dc.runStudent(1000, 40)
 data = np.load('data.npy', allow_pickle=True)
 
 
@@ -33,6 +33,12 @@ def transitTime():
 def drawPicture(i):
     label = 'timestep {0}'.format(i)
     newData = transitTime()
+    ax1.scatter3D(37948.54209741189, 39.5, 58500.81314611783, s=200, c='darkred', marker='^', linewidths=1)
+    # ax1.text(38948.54209741189 + 3000, 41 + 3, 58422.81314611783 + 3000, '培养达标阈值', fontsize=15)
+    ax1.text(37948.54209741189 + 3500, 39.5 + 2, 57422.81314611783 + 3000, '培养达标阈值', fontsize=13)
+    ax1.text(45000, 23, 0, '传统金融专业学生', fontsize=13)
+    ax1.text(0, 30, 20000, '传统计算机专业学生', fontsize=13)
+    ax1.text(37948.54209741189 + 3500, 21, 47422.81314611783 + 3000, '金融科技专业学生', fontsize=13)
     for s in range(len(newData)):
         ax1.plot3D(newData[s][:, 0], newData[s][:, 2], newData[s][:, 1])
         # ax1.plot3D(newData[s][i, 0], newData[s][i, 2], newData[s][i, 1])
@@ -41,7 +47,7 @@ def drawPicture(i):
     ax1.set_xlabel('业务理解能力')
     ax1.set_zlabel('专业工具应用能力')
     ax1.grid(True)
-    ax1.view_init(10, 50)
+    ax1.view_init(10, 30)
     fig.savefig('Simulation.png')
     plt.show()
     return ax1
